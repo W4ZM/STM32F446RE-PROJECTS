93b5b109c0a7f7d088fc22851f18768ac22a2a64
W4ZM <82101268+W4ZM@users.noreply.github.com>
2025-09-18 16:44:41 +0100

    added some comments

--- src/main.c
+++ src/main.c
@@ -5,13 +5,21 @@
 
 void main(void){
   
+  // enable GPIOA clock.
   RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOAEN_Pos);
-  GPIOA->MODER |= (1 << GPIO_MODER_MODER5_Pos);
-  
+
+  // ensure the clock is really stable before accessing the peripheral registers.
+  volatile uint32_t dummy;
+  dummy = RCC->AHB1ENR;
+  dummy = RCC->AHB1ENR;
+
+  // configure PA5 as output.
+  GPIOA->MODER |= (1 << GPIO_MODER_MODER5_Pos)
+
+  // toggle PA5 in an infinite loop with a delay.
   while(1)
   {
     GPIOA->ODR ^= (1 << LED_PIN);    
     for (uint32_t i = 0; i < 1000000; i++);
   }
-
 }
